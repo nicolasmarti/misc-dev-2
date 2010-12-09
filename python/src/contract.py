cbc91c44db4a194cd1d84eaeb6d8598854fd2b64
Nicolas Marti <nicolas.marti.japon@gmail.com>
2010-12-09 23:11:27 +0900

    update

--- python/src/contract.py
+++ python/src/contract.py
@@ -42,6 +42,7 @@ class Stock:
         self.mktDataId = self.con.reqMktData(self.contract, False)
         self.mktDepthId = self.con.reqMktDepth(self.contract, 10)
 
+        self.oids = []
 
     def __del__(self):
         
@@ -52,37 +53,77 @@ class Stock:
         return "Contract"
 
     def __getattr__(self, name):
-        if name == "mktdata":
-            return self.con.getMktData(self.mktDataId)
-        elif name == "mktdepth":
-            return self.con.getMktDepth(self.cancelMktDepth)
+        if name == "order":
+            def order(position = "BUY", orderty = "MKT", quantity = 100, price = None, timeout = None, openClose = "O"):
+                order = Order()
+                order.m_action = position
+                order.m_tif = "DAY"
+                order.m_orderType = orderty
+                order.m_totalQuantity = quantity
+                order.m_openClose = openClose
+                if price <> None: order.m_lmtPrice = float(round(float(price)))
+                
+                if timeout == None:
+                    oid = self.con.placeOrder(self.contract, order, None)
+                    self.oids.append(oid)
+                    return oid
+                else:
+                    oid = self.con.placeOrder(self.contract, order, timedelta(seconds=timeout))
+                    if oid == None:
+                        return None
+                    else:
+                        self.oids.append(oid)
+                        return oid
+            return order
+        elif name == "close":
+            def close(orderty = "MKT", price = None, timeout = None):
+                position = self["position"]
+                order = Order()
+                if position > 0:
+                    order.m_action = "SELL"
+                else:
+                    order.m_action = "BUY"
+                order.m_tif = "DAY"
+                order.m_orderType = orderty
+                order.m_totalQuantity = position
+                order.m_openClose = "C"
+                if price <> None: order.m_lmtPrice = float(round(float(price)))
+                
+                if timeout == None:
+                    oid = self.con.placeOrder(self.contract, order, None)
+                    self.oids.append(oid)
+                    return oid
+                else:
+                    oid = self.con.placeOrder(self.contract, order, timedelta(seconds=timeout))
+                    if oid == None:
+                        return None
+                    else:
+                        self.oids.append(oid)
+                        return oid
+            return close
 
-        return None
+        else:
+            return None
 
     def __getitem__(self, key):
         if key == "mktdata":
             return self.con.getMktData(self.mktDataId)
         elif key == "mktdepth":
             return self.con.getMktDepth(self.cancelMktDepth)
-
-    def __call__(self, position = "BUY", orderty = "MKT", quantity = 100, price = None, timeout = None, openClose = "O"):
-        order = Order()
-        order.m_action = position
-        order.m_tif = "DAY"
-        order.m_orderType = orderty
-        order.m_totalQuantity = quantity
-        order.m_openClose = openClose
-        if price <> None: order.m_lmtPrice = float(round(float(price)))
-
-        if timeout == None:
-            return self.con.placeOrder(self.contract, order, None)
-        else:
-            return self.con.placeOrder(self.contract, order, timedelta(seconds=timeout))
+        elif key == "orders":
+            l = []
+            for i in self.oids:
+                l.append(self.con.orderStatus(i))
+            return l
+        elif key == "position":
+            position = 0
+            for i in self.oids:
+                if len(self.con.orderStatus(i)[1]) > 2:
+                    if self.con.orderStatus(i)[0][1].m_action == "BUY": 
+                        mul = 1
+                    else:
+                        mul = -1
+                    position += mul * self.con.orderStatus(i)[1][2]
+            return position
     
-
-    def getNextId(self):
-        self.m_lock.acquire()
-        res = self.m_nextid
-        self.m_nextid += 1
-        self.m_lock.release()
-        return res
+    
