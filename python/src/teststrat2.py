0a1210b01432f7f90d42981894775ba92dac364a
Nicolas Marti <nicolas.marti.japon@gmail.com>
2010-12-21 22:45:29 +0900

    simple test

--- python/src/teststrat2.py
+++ python/src/teststrat2.py
@@ -1,8 +1,32 @@
 from time import *
-from indicator import *
+from indicators import *
 from contract import *
 
 c = Stock("GS")
 
+ema20 = EMAIndicator(20, "close")
+ema50 = EMAIndicator(50, "close")
 
+ema20upema50 = ema20.croseup(ema50)
+ema50upema20 = ema50.croseup(ema20)
 
+while True:
+    sleep(10)
+
+    res1 = ema20upema50(c.bars)
+    res2 = ema50upema20(c.bars)
+
+    if res1 <> "None" and res1 == True:
+        c.order()
+
+    if res2 <> "None" and res2 == True:
+        c.close()
+
+    c.newbar()
+
+    print "res1 = " + str(res1)
+    print "res2 = " + str(res2)
+    print "position = " + str(c["position"])
+    print "upnl = " + str(c["upnl"])
+    print "rpnl = " + str(c["rpnl"])
+    print "pnl = " + str(c["rpnl"] + c["upnl"])
