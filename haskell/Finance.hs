755fdea42e411830fe62022bd4aa3b3e3a478ada
Nicolas Marti <nicolas.marti.japon@gmail.com>
2011-02-13 10:19:24 +0900

    just a starter

---
+++ haskell/Finance.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE ExistentialQuantification, FlexibleInstances, RankNTypes, ScopedTypeVariables, FlexibleInstances, MultiParamTypeClasses, FunctionalDependencies, TypeSynonymInstances, UndecidableInstances, EmptyDataDecls #-}
+
+module Finance where 
+
+data Model
+
+data Contract
+
+data OHLCV
+
+data TSer
+
+data Pattern
+
+data Strategy
