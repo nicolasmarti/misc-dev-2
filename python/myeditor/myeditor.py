47703a19a4c8f17a6c4cb9bee3e03a6e617e3ce1
Nicolas Marti <nicolas@nicolas-desktop.(none)>
2011-06-08 14:19:04 +0900

    start python editor

---
+++ python/myeditor/myeditor.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import sets
+
+class MyBuffer(gtk.TextBuffer):
+
+    def input_key(self, pressed_key):
+        pass
+
+    def __init__(self):
+        
+        gtk.TextBuffer.__init__(self)
+
+        self.pressed_buffer = []
+
+
+class MyView(gtk.TextView):
+
+    def key_pressed(self, widget, event, data=None):
+        self.pressed_key.add(event.keyval)
+        print "pressed: " + str(self.pressed_key)
+
+
+    def key_released(self, widget, event, data=None):
+        self.pressed_key.remove(event.keyval)
+        print "pressed: " + str(self.pressed_key)
+
+    def __init__(self):
+        
+        gtk.TextView.__init__(self)
+
+        # not editable: we grab things ... 
+        self.set_editable(False)
+
+        self.pressed_key = sets.Set()
+
+        self.connect("key_press_event", self.key_pressed, None)
+        self.connect("key_release_event", self.key_released, None)
+
+
+class MyEditor:
+    
+    def close_application(self, widget):
+        gtk.main_quit()
+
+    def __init__(self):
+        
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_resizable(True)  
+        window.connect("destroy", self.close_application)
+        window.set_title("TextView Widget Basic Example")
+        window.set_border_width(0)
+
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        textview = MyView()
+        textbuffer = textview.get_buffer()
+        sw.add(textview)
+        sw.show()
+        textview.show()
+
+        window.add(sw)
+        sw.show()
+
+        window.resize(800, 600)
+
+        window.show()
+
+def main():
+    gtk.main()
+    return 0       
+
+if __name__ == "__main__":
+    MyEditor()
+    main()
+
