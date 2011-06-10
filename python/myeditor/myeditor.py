828e9294bc7c2ae11a6fd189ce726c55d9d92f09
Nicolas Marti <nicolas@nicolas-desktop.(none)>
2011-06-10 12:14:46 +0900

    continue :D

--- python/myeditor/myeditor.py
+++ python/myeditor/myeditor.py
@@ -3,30 +3,76 @@
 import pygtk
 pygtk.require('2.0')
 import gtk
-import sets
+from sets import *
+from types import *
+
 
 class MyBuffer(gtk.TextBuffer):
 
-    def input_key(self, pressed_key):
+    def managekey(self, s):
         pass
 
     def __init__(self):
         
         gtk.TextBuffer.__init__(self)
 
+        self.set_text("doudou")
+
         self.pressed_buffer = []
 
+# :: [(list (set int), void (MyView))]
+keysemantics = [
+    ([Set([65507, 120]), Set([48])],
+     lambda mv: mv.myframe.undivide()
+     ),
+    ([Set([65507, 120]), Set([50])],
+     lambda mv: mv.myframe.divideV()
+     ),
+    ([Set([65507, 120]), Set([51])],
+     lambda mv: mv.myframe.divideH()
+     ),
+    ([Set([65507, 120]), Set([65507, 99])],
+     lambda mv: gtk.main_quit()
+     )
+    ]
 
 class MyView(gtk.TextView):
 
+    def managekey(self):
+        valid = False
+        for i in keysemantics:
+            if len(i[0]) > len(self.validkeysequences):
+                prefix = True
+                for j in range(0, len(self.validkeysequences) - 1):
+                    prefix = prefix and (self.validkeysequences[j] == i[0][j])
+                    
+                if prefix:
+                    if self.pressed_key == i[0][len(self.validkeysequences)]:
+                        self.validkeysequences.append(self.pressed_key)
+                        if len(self.validkeysequences) == len(i[0]):
+                            self.validkeysequences = []
+                            i[1](self)
+                            return
+                        else:
+                            valid = True
+                    elif self.pressed_key <= i[0][len(self.validkeysequences)] and len(self.validkeysequences) > 0:
+                        valid = True
+        
+        if not valid:
+            if len(self.validkeysequences) == 0:
+                self.get_buffer().managekey(self.pressed_key)
+                
+            self.validkeysequences = []
+
+
     def key_pressed(self, widget, event, data=None):
+        print "pressed: " + str((event.keyval, event.string, event.state))
         self.pressed_key.add(event.keyval)
-        print str(self) + "pressed: " + str(self.pressed_key)
+        self.managekey()
 
 
     def key_released(self, widget, event, data=None):
-        self.pressed_key.remove(event.keyval)
-        print str(self) + "pressed: " + str(self.pressed_key)
+        self.pressed_key.discard(event.keyval)
 
     def __init__(self, buffer):
         
@@ -36,7 +82,7 @@ class MyView(gtk.TextView):
         self.set_editable(False)
 
         # the set of pressed keys
-        self.pressed_key = sets.Set()
+        self.pressed_key = Set()
 
         # the key handlers
         self.connect("key_press_event", self.key_pressed, None)
@@ -44,8 +90,8 @@ class MyView(gtk.TextView):
 
         self.show()
 
-        buffer.set_text("doudou")
-
+        # :: [Set (int)]
+        self.validkeysequences = []
 
 
 class MyFrame(gtk.Frame):
@@ -58,6 +104,14 @@ class MyFrame(gtk.Frame):
         if self.mode == "SPLITTED":
             return self.inside.get_child1().getTextBuffer()
 
+    def getTextView(self):
+
+        if self.mode == "PLAIN":
+            return self.inside
+        
+        if self.mode == "SPLITTED":
+            return self.inside.get_child1().getTextView()
+
     def divideH(self):
 
         textbuf = self.getTextBuffer()
@@ -65,7 +119,10 @@ class MyFrame(gtk.Frame):
         self.remove(self.inside)
 
         child1 = MyFrame(self.inside, self.mode)
+        child1.myframe = self
+
         child2 = MyFrame(MyView(textbuf))
+        child2.myframe = self
 
         panel = gtk.HPaned()
 
@@ -77,6 +134,8 @@ class MyFrame(gtk.Frame):
         self.inside = panel
         self.mode = "SPLITTED"
 
+        child1.getTextView().grab_focus()
+
     def divideV(self):
 
         textbuf = self.getTextBuffer()
@@ -84,7 +143,10 @@ class MyFrame(gtk.Frame):
         self.remove(self.inside)
 
         child1 = MyFrame(self.inside, self.mode)
+        child1.myframe = self
+
         child2 = MyFrame(MyView(textbuf))
+        child2.myframe = self
 
         panel = gtk.VPaned()
 
@@ -96,10 +158,27 @@ class MyFrame(gtk.Frame):
         self.inside = panel
         self.mode = "SPLITTED"
 
+        child1.getTextView().grab_focus()
+
+    def setMyFrame(self, frame):
+        if self.mode == "SPLITTED":
+            child1 = self.inside.get_child1()
+            child1.myframe = frame
+
+            child2 = self.inside.get_child2()
+            child2.myframe = frame
+
+            return
+
+        if self.mode == "PLAIN":
+            self.myframe = frame
+
     def undivide(self):
 
         if self.mode == "SPLITTED":
             child1 = self.inside.get_child1()
+            child1.setMyFrame(self)
+
             child2 = self.inside.get_child2()
             
             self.inside.remove(child1)
@@ -109,8 +188,36 @@ class MyFrame(gtk.Frame):
             self.remove(self.inside)
 
             self.inside = child1.inside
+            child1.inside.myframe = self
             self.add(child1.inside)
             self.mode = child1.mode
+
+            child1.getTextView().grab_focus()
+            return
+
+        if self.mode == "PLAIN" and self.myframe <> self:
+            self.myframe.undivide()
+
+    def tostring(self):
+        if self.mode == "PLAIN":
+
+            if self.inside.myframe <> self:
+                raise Exception('child wrong !!!')
+
+            return "(" + str(self) + ")"
+            
+        if self.mode == "SPLITTED":
+            child1 = self.inside.get_child1()
+
+            if child1.myframe <> self:
+                raise Exception('left child wrong !!!')
+
+            child2 = self.inside.get_child2()
+
+            if child2.myframe <> self:
+                raise Exception('left child wrong !!!')
+
+            return child1.tostring() + " <--(" + str(self) + ")--> " + child2.tostring()
 
     def __init__(self, inside, mode = "PLAIN"):
 
@@ -120,6 +227,13 @@ class MyFrame(gtk.Frame):
         self.show()
 
         self.mode = mode
+
+        if mode == "PLAIN":
+            self.inside.myframe = self
+
+        self.myframe = self
+
+        
 
 
 class MyEditor:
@@ -141,11 +255,11 @@ class MyEditor:
         textbuffer = MyBuffer()
         textview = MyView(textbuffer)
         mainFrame = MyFrame(textview)
-        
+
         window.add(mainFrame)
         
-        mainFrame.divideH()
-        mainFrame.divideV()
+        #mainFrame.divideH()
+        #mainFrame.divideV()
 
         #mainFrame.undivide()
         #mainFrame.undivide()
@@ -153,6 +267,15 @@ class MyEditor:
         window.resize(800, 600)
 
         window.show()
+
+        def printmainframe(frame):
+            print frame.tostring()
+
+        keysemantics.append( 
+            ([Set([65507, 120]), Set([113])],
+             lambda mv: printmainframe(mainFrame)
+             )
+            )
 
 def main():
     gtk.main()
