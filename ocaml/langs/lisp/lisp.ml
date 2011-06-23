c56446d919c5ace20afefc295d64a581d9fb8ac9
Nicolas Marti <nicolas@nicolas-desktop.(none)>
2011-06-23 15:05:17 +0900

    + example

--- ocaml/langs/lisp/lisp.ml
+++ ocaml/langs/lisp/lisp.ml
@@ -1488,3 +1488,21 @@ let _ = interp_exprs "
      
      (keep-three-letter-words '(one two three four five six))
 ";;
+
+let _ = interp_exprs "
+(defun triangle-initialization (number)
+       \"Return the sum of the numbers 1 through NUMBER inclusive.
+     This is the `initialization' component of a two function
+     duo that uses recursion.\"
+       (triangle-recursive-helper 0 0 number))
+     (defun triangle-recursive-helper (sum counter number)
+       \"Return SUM, using COUNTER, through NUMBER inclusive.
+     This is the `helper' component of a two function duo
+     that uses recursion.\"
+       (if (> counter number)
+           sum
+         (triangle-recursive-helper (+ sum counter)  ; sum
+                                    (1+ counter)     ; counter
+                                    number)))        ; number
+(triangle-initialization 2)
+";;
