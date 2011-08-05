c2b1e7bf8c90ed8010c363ad60f688541f2a7537
Nicolas Marti <nicolas@nicolas-desktop.(none)>
2011-08-05 11:52:26 +0900

    fixes the test

--- ocaml/tests/test.lisp
+++ ocaml/tests/test.lisp
@@ -1,9 +1,10 @@
-(setq animals '(gazelle giraffe lion tiger)) 
-   
+(setq animals '(gazelle giraffe lion tiger))
+
 (defun print-elements-of-list (list)
-       \"Print each element of LIST on a line of its own.\"
+       "Print each element of LIST on a line of its own."
        (while list
-         (message \"%s\n\" (car list))
+         (message "%s\n" (car list))
          (setq list (cdr list))))
 
+   
 (print-elements-of-list animals)
