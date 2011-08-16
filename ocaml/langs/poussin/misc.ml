d64c5dafbdeb26f21767687622e9afffc352917e
Nicolas Marti <nicolas@nicolas-desktop.(none)>
2011-08-17 08:41:07 +0900

    remove poussin ... let's try to do ONE thing at the time

--- ocaml/langs/poussin/misc.ml
+++
@@ -1,5 +0,0 @@
-(* some helpers functions *)
-
-(* compute the set of a term free variables *)
-let rec term_fvs (te: term) : IndexSet =
-  raise (Failure "NYI")
--- ocaml/langs/poussin/poussin.ml
+++
@@ -1,7 +0,0 @@
-(* a summarize of all needed modules *)
-
-(* all the data structures definitions *)
-open Datastructures
-
-(* the typechecking (which include reduction, unification, type inference) *)
-open Typecheck
