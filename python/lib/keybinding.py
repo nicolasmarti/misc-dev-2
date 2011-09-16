face9a1f39c03605aee117d6e7c9e41b6baabd44
Nicolas Marti <nicolas@nicolas-desktop.(none)>
2011-09-16 09:08:46 +0900

    a class for emulation of emacs keybinding

---
+++ python/lib/keybinding.py
@@ -0,0 +1,77 @@
+# this class is supposed to somehow emulate an emacs inputs of key
+# this is supposed to be derived
+
+
+class KeyBinding:
+
+    def __init__(self):
+        
+        # the set of pressed keys
+        self.pressed_key = Set()
+
+        # the set of keybinding
+        # basically a [([Set keycode]), self -> ()]
+        self.keyactions = []
+
+        # the latest longest valid key sequences
+        self.validkeysequences = []
+
+        # there is a special keycode for control
+        self.ctrl = 0
+        
+
+    # this function is call when a key is pressed
+    def keypressed(self, keycode):
+        # we add the key pressed to the set
+        self.pressed_key.add(event.keyval)
+        # and call the manager
+        self.managekey()
+
+    # this function is call when a key is released
+    def keyreleased(self, keycode):
+        # discard the key from the set
+        self.pressed_key.discard(event.keyval)
+
+
+    # the key manager: decide if the current list of entered key is a valid one,
+    # and run the action
+    def managekey(self):
+
+        # initially the validity id false
+        valid = False
+        # we do a traversal of all the key actions
+        for i in self.keyactions:
+            # if the valid keysequence so far is not empty
+            if len(i[0]) > len(self.validkeysequences):
+                # we make a fold that will check if the sequence is a prefix of the currently checked keyactions
+                prefix = True
+                for j in range(0, len(self.validkeysequences) - 1):
+                    prefix = prefix and (self.validkeysequences[j] == i[0][j])
+                    
+                # yes this is a prefix
+                if prefix:
+                    # is the pressed key is the following sequence we were waiting for
+                    if self.pressed_key == i[0][len(self.validkeysequences)]:
+                        # append to validsequence
+                        self.validkeysequences.append(self.pressed_key)
+                        # if if this is the final one 
+                        if len(self.validkeysequences) == len(i[0]):
+                            # then we can execute the action
+                            # we reset the sequence
+                            self.validkeysequences = []
+                            # clear the presed key
+                            self.pressed_key = Set()
+                            # and call the actions
+                            i[1](self)
+                            return
+                        else:
+                            # if its not the final, this is yet a valid pressed key
+                            valid = True
+                    # if the pressed_keys are a subset for what we are waiting, we also say this is/(might be) valid
+                    elif self.pressed_key <= i[0][len(self.validkeysequences)] and (len(self.validkeysequences) > 0 or Set([self.ctrl]) <= self.pressed_key):
+                        valid = True
+
+        # it is not a valid sequence
+        if not valid:
+            # we reset the validkeysequence
+            self.validkeysequences = []
